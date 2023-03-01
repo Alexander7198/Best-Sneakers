4dbda04752a87d2c195e009e412a80d405fed4f4
Alexander Dorofeev <alexandedorofeev@yandex.ru>
2023-03-02 02:25:45 +0300

    two commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2>Корзина</h2>
+
+          <div className="cartItem d-flex align-center">
+
+            <div
+              style={{ backgroundImage: 'url(/img/sneakers/1.png)' }}
+              className="cartItemImg"></div>
+
+            <div className="mr-20 flex">
+              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+              <b>8 499 руб.</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-remove.jpg" alt="Remove" />
+          </div>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40" >
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.png" />
@@ -22,10 +41,19 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">Все кроссовки</h1>
+        <div className="d-flex align-center justify-between mb-40 " >
+          <h1>Все кроссовки</h1>
+          <div className="search-block">
+            <img src="/search1.jpg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
 
         <div className="d-flex">
           <div className="card">
+            <div className="favorite">
+              <img src="/img/heart-unlike.jpg"></img>
+            </div>
             <img width={133} height={112} src="/img/sneakers/1.png" alt="Sneakers" />
             <h5>Кроссовки Puma X Aka Boku Future Rider</h5>
             <div className="d-flex justify-between">
